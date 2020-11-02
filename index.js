d21c352ee82e053abc0966e232daed9efb4cfafe
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-11-02 15:09:01 +0100

    solve

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Read instructions. And remove this line.
+function randomInt(n){
+    return Math.floor(Math.random() * n) + 1
+}
+
+const randomNumber = randomInt(100)
+console.log(randomNumber)
+
+function getUserGuess(){
+    const stringValue = document.getElementById("user-input").value
+    return parseInt(stringValue, 10)
+}
+
+document.addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+        if (randomNumber === getUserGuess()){
+            setMessage('Correct!')
+        } else if (randomNumber > getUserGuess()) {
+            setMessage('Too low!')
+        } else if (randomNumber < getUserGuess()) {
+            setMessage('Too high!')
+        }
+        document.getElementById('user-input').value = ''
+    }
+})
+
+function setMessage(msg){
+    document.getElementById("message").innerText = msg
+}

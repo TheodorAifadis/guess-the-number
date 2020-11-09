67720c53a2e1b5dc3fc3600c1c3bbb0a1adcdd28
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-11-09 13:51:36 +0100

    commit5

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
+let count = 0
+const guesses = []
+let randomNumber = randomInt(100)
+const resetButton = document.getElementsByTagName('button')[0]
+
 function randomInt(n){
     return Math.floor(Math.random() * n) + 1
 }
-
-const randomNumber = randomInt(100)
-console.log(randomNumber)
 
 function getUserGuess(){
     const stringValue = document.getElementById("user-input").value
     return parseInt(stringValue, 10)
 }
 
+resetButton.addEventListener('click', resetGame)
+
 document.addEventListener('keyup', function (event) {
     if (event.key === 'Enter') {
+        guesses.push(getUserGuess)
+        count++ 
         if (randomNumber === getUserGuess()){
-            setMessage('Correct!')
+            setMessage('Correct!     ' +  count + '     attempts')
         } else if (randomNumber > getUserGuess()) {
             setMessage('Too low!')
         } else if (randomNumber < getUserGuess()) {
@@ -25,4 +31,12 @@ document.addEventListener('keyup', function (event) {
 
 function setMessage(msg){
     document.getElementById("message").innerText = msg
+}
+
+function resetGame() {
+    guesses.length = 0
+    count = 0
+    randomNumber = randomInt(100)
+    document.getElementById('user-input').value = ''
+    setMessage('Guess a number between 0 and 100')
 }
